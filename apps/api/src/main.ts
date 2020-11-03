73f97e3ee9eba0a4d8cdd9ef09e4dd709e1e6427
Gérard Paschke <gerard.paschke@lhind.dlh.de>
2020-11-03 22:36:03 +0100

    Add Swagger UI to api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,12 +7,24 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  // Swagger UI
+  const options = new DocumentBuilder()
+    .setTitle('API example')
+    .setDescription('Custom API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(globalPrefix, app, document);
+
+
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
