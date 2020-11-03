1617bef794a67c793427408e1c02b493480d5377
Gérard Paschke <gerard.paschke@lhind.dlh.de>
2020-11-03 23:34:52 +0100

    Remove Swagger UI from deployment
    - may add PORT=80 variable to deployment stages

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,7 +7,6 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,15 +14,19 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
 
-  // Swagger UI
-  const options = new DocumentBuilder()
-    .setTitle('API example')
-    .setDescription('Custom API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup(globalPrefix, app, document);
-
+  // Swagger UI - dynamic import
+  if(!process.env.PORT) {
+    console.log('process.env.PORT');
+    console.log(process.env.PORT);
+    const sw = await import('@nestjs/swagger')
+    const options = new sw.DocumentBuilder()
+      .setTitle('API example')
+      .setDescription('Custom API description')
+      .setVersion('1.0')
+      .build();
+    const document = sw.SwaggerModule.createDocument(app, options)
+    sw.SwaggerModule.setup(globalPrefix, app, document)
+  }
 
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
